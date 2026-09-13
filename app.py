e1270d74c4bfe6d302ac55e843694c53cc89a25c
kamilkozlowski113-tech <kamilkozlowski113@gmail.com>
2026-09-13 13:09:55 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -11,13 +11,13 @@ def index():
     # Pobieranie filtrów z formularza
     selected_style = request.args.get('style', '')
     selected_day = request.args.get('day', '')
-    
+
     filtered_profiles = profiles
     if selected_style:
         filtered_profiles = [p for p in filtered_profiles if p['style'] == selected_style]
     if selected_day:
         filtered_profiles = [p for p in filtered_profiles if p['day'] == selected_day]
-        
+
     return render_template('index.html', profiles=filtered_profiles, selected_style=selected_style, selected_day=selected_day)
 
 @app.route('/add', methods=['POST'])
@@ -35,6 +35,12 @@ def add_profile():
     profiles.append(new_profile)
     return redirect(url_for('index'))
 
+@app.route('/delete/<int:profile_id>', methods=['POST'])
+def delete_profile(profile_id):
+    global profiles
+    profiles = [p for p in profiles if p['id'] != profile_id]
+    return redirect(url_for('index'))
+
 if __name__ == '__main__':
-  port = int(os.environ.get('PORT', 10000))
-  app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
